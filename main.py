777732ba5bae9e928fadab1359bb4a63eb6394c6
hack-system-ai <hack.system.ai@gmail.com>
2025-05-15 19:52:17 +0800

    update-file

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import cv2
 from ultralytics import YOLO
-import time
-import numpy
 import threading
+import queue
+import time
+
+frame_queue = queue.Queue(timeout=2)
+result_queue = queue.Queue(timeout=2)
+
+def capture_threading(cap, frame_queue):
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
